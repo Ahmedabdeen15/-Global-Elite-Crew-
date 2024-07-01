37eb83ae3f95bba8a9f579a95da530da8a193e6f
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-07-01 03:50:48 +0300

    init index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,62 @@
+/**---------------nav-bar-----------------*/
 document.addEventListener('DOMContentLoaded', function() {
-    var navbarToggler = document.querySelector('.navbar-toggler');
-    var navbarCollapse = document.querySelector('#navbarSupportedContent');
-    var navCover = document.querySelector('.nav-cover');
-    var navElement = document.querySelector('nav');
+    if (window.innerWidth < 768) {
+        var navbarToggler = document.querySelector('.navbar-toggler');
+        var navbarCollapse = document.querySelector('#navbarSupportedContent');
+        var navCover = document.querySelector('.nav-cover');
+        var navElement = document.querySelector('nav');
 
-    navbarToggler.addEventListener('click', function() {
-        if (navbarCollapse.classList.contains('show')) {
-            navCover.style.height = '95px'; // Reset to default height
-            navElement.style.height = '95px'; // Reset to default height
-            navElement.style.borderRadius = '0px 0px 160px 160px'; // Reset border-radius
-        } else {
-            navCover.style.height = '45vh'; // Expand to full viewport height
-            navElement.style.height = '45vh'; // Expand to full viewport height
-            navElement.style.borderRadius = '0px 0px 90px 90px'; // Remove border-radius for full height
+        navbarToggler.addEventListener('click', function() {
+            if (navbarCollapse.classList.contains('show')) {
+                navCover.style.height = '95px'; // Reset to default height
+                navElement.style.height = '95px'; // Reset to default height
+                navElement.style.borderRadius = '0px 0px 160px 160px'; // Reset border-radius
+            } else {
+                navCover.style.height = '45vh'; // Expand to full viewport height
+                navElement.style.height = '45vh'; // Expand to full viewport height
+                navElement.style.borderRadius = '0px 0px 90px 90px'; // Remove border-radius for full height
+            }
+        });
+
+        // Ensure nav-cover and nav reset height when navbar is collapsed
+        navbarCollapse.addEventListener('hidden.bs.collapse', function () {
+            navCover.style.height = '95px';
+            navElement.style.height = '95px';
+            navElement.style.borderRadius = '0px 0px 80px 80px';
+        });
+    }
+});
+/**---------------slider-----------------*/
+$(document).ready(function(){
+    $('.past-projects-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 1000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
         }
     });
+  });
 
-    // Ensure nav-cover and nav reset height when navbar is collapsed
-    navbarCollapse.addEventListener('hidden.bs.collapse', function () {
-        navCover.style.height = '95px';
-        navElement.style.height = '95px';
-        navElement.style.borderRadius = '0px 0px 80px 80px';
-    });
-});
 
+/**---------------map-----------------*/
 var map = L.map('map').setView([31.211273, 29.945373], 15);
 googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
